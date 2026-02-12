0f36bbfd6b9804a2416484271c140cb8e58ac421
SACHINNXD <145748982+SACHINNXD@users.noreply.github.com>
2026-02-12 11:49:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-window.addEventListener("load", function () {
-
-  const loader = document.getElementById("loader");
-  const main = document.getElementById("main-content");
-
-  if (sessionStorage.getItem("loaderShown")) {
-    loader.style.display = "none";
-    main.classList.remove("hidden");
-  } else {
-    setTimeout(() => {
-      loader.style.display = "none";
-      main.classList.remove("hidden");
-      sessionStorage.setItem("loaderShown", "true");
-    }, 2000);
-  }
-
-});
+console.log("Website Loaded Successfully");
