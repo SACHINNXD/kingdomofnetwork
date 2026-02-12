d30b8b4f526be3a2db1ee6012b8097dff2d272b8
SACHINNXD <145748982+SACHINNXD@users.noreply.github.com>
2026-02-12 11:43:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Website Loaded Successfully");
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("main-content").classList.remove("hidden");
+  }, 2000); // 2 seconds
+});
